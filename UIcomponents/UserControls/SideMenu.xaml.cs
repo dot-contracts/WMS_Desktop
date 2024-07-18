607e195a68d6817ba09ce288d00898684018faf4
dot-contracts <2000kibethipaul@gmail.com>
2024-07-18 13:55:24 +0300

    Desktop application structure

---
+++ UIcomponents/UserControls/SideMenu.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FicmaDT.UIcomponents.UserControls
+{
+    public partial class SideMenu : UserControl
+    {
+        public SideMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle the button click event here
+            Button clickedButton = sender as Button;
+            if (clickedButton != null)
+            {
+                MessageBox.Show($"Button clicked: {clickedButton.Content}");
+            }
+        }
+    }
+}
