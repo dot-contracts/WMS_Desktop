86ee93dce8c549d517658edc25cd064b8b934580
dot-contracts <2000kibethipaul@gmail.com>
2024-07-23 20:44:04 +0300

    modified app.xaml class for postgres connection

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,13 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using FicmaDT.UIcomponents.UserControls;
+using FicmaDT.Data;
 
 namespace FicmaDT
 {
@@ -9,6 +16,49 @@ namespace FicmaDT
     /// </summary>
     public partial class App : Application
     {
+        private IHost _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    // Read configuration from appsettings.json
+                    var configuration = new ConfigurationBuilder()
+                        .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .Build();
+
+                    services.AddSingleton<IConfiguration>(configuration);
+
+                    // Register DbContext with PostgreSQL
+                    services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+                    // Register other services
+                    services.AddTransient<MainWindow>();
+                    services.AddTransient<SideMenu>();
+                    services.AddTransient<Home>();
+                    services.AddTransient<BulkSms>();
+                    services.AddTransient<Dispatch>();
+                    services.AddTransient<Invoice>();
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _host.Dispose();
+            base.OnExit(e);
+        }
     }
 
 }
