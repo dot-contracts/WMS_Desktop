c36d5727a37d734ee26b8ba91f8ad1da9bcb8990
dot-contracts <2000kibethipaul@gmail.com>
2024-07-23 20:41:57 +0300

    Added DbContext

---
+++ Data/DbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FicmaDT.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet properties for your entities
+        public DbSet<YourEntity> YourEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Customize your model with Fluent API here
+        }
+    }
+
+    public class YourEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Other properties
+    }
+}
